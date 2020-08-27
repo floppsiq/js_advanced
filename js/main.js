26a6e980959499373db5c19622cb111cb6f1fd66
floppsiq <66955949+floppsiq@users.noreply.github.com>
2020-08-27 13:15:25 +0700

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,59 +1,90 @@
+const API = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses';
+
+// let getRequest = (url) => {
+//     return new Promise((resolve, reject) => {
+//         let xhr = new XMLHttpRequest();
+//         // window.ActiveXObject -> xhr = new ActiveXObject()
+//         xhr.open("GET", url, true);
+//         xhr.onreadystatechange = () => {
+//             if(xhr.readyState === 4){
+//                 if(xhr.status !== 200){
+//                     reject('Error');
+//                 } else {
+//                     resolve(xhr.responseText);
+//                 }
+//             }
+//         };
+//         xhr.send();
+//     })
+// };
+//
+// getRequest('tel.json').then(data => {
+//
+// })
+
 class ProductList {
+  #goods;
 
   constructor(container = '.products') {
     this.container = container;
-    this.goods = [];
+    this.#goods = [];
     this.allProducts = [];
-    this.fetchProducts();
-    this.render();
+    // this.#fetchGoods();
+    this.#getProducts()
+        .then(data => {
+          this.#goods = [...data];
+          this.#render();
+        });
   }
 
-  fetchProducts() {
-    this.goods = [
-      {id: 1, title: 'Notebook', price: 20000},
-      {id: 2, title: 'Mouse', price: 1500},
-      {id: 3, title: 'Keyboard', price: 5000},
-      {id: 4, title: 'Gamepad', price: 4500},
-    ];
+  // #fetchGoods() {
+  //   getRequest(`${API}/catalogData.json`, (data) => {
+  //     this.#goods = JSON.parse(data);
+  //     this.#render();
+  //   });
+  // }
+  #getProducts() {
+    return fetch(`${API}/catalogData.json`)
+        .then(result => result.json())
+        .catch(error => {
+          console.log(error);
+        });
   }
 
-  render() {
+  #render() {
     const block = document.querySelector(this.container);
 
-    for (let product of this.goods) {
+    for (let product of this.#goods) {
       const productObject = new ProductItem(product);
 
       this.allProducts.push(productObject);
       block.insertAdjacentHTML('beforeend', productObject.render());
     }
-	this.total();
   }
-  total() {
-	const block = document.querySelector('.products__sum');
-	let totalPrice = 0;
-	for (let good of this.goods) {
-	  totalPrice += good.price;
-	}
-	//console.log(totalPrice);
-	block.innerText = `Общая стоимость товаров: ${totalPrice} рублей`;
+
+  getGoodsPrice() {
+    return this.#goods.reduce((sumPrice, { price }) => sumPrice + price, 0);
   }
 }
 
 class ProductItem {
-  constructor(product, img = 'https://liftshop24.ru/custom/custom31_07_2018_15_15/img/not-image.png') {
-    this.title = product.title;
+  constructor(product, img='https://liftshop24.ru/custom/custom31_07_2018_15_15/img/not-image.png') {
+    this.product_name = product.product_name;
     this.price = product.price;
-    this.id = product.id;
+    this.id_product = product.id_product;
     this.img = img;
   }
 
   render() {
     return `
           <div class="product__item">
-            <img class="product__img" src="${this.img}" alt="${this.title}" />
-            <h3 class="product__title">${this.title}</h3>
-            <p class="product__title">${this.price} \u20bd</p>
-            <button data-id="${this.id}" class="btn btn__add">Добавить в корзину</button>
+            <img class="product__img" src="${this.img}" alt="${this.product_name}" />
+            <h3 class="product__title">${this.product_name}</h3>
+            <p class="product__price">${this.price} \u20bd</p>
+            <button class="btn btn__add"
+					data-id="${this.id_product}"
+                    data-name="${this.product_name}"
+                    data-price="${this.price}">Добавить в корзину</button>
           </div>`;
   }
 }
