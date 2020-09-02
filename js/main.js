32bc3606104c9f7d105558e111bd888db45ebf31
floppsiq <66955949+floppsiq@users.noreply.github.com>
2020-09-02 23:01:53 +0700

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,25 +1,59 @@
-const products = [
-  {id: 1, title: 'Notebook', price: 20000},
-  {id: 2, title: 'Mouse', price: 1500},
-  {id: 3, title: 'Keyboard', price: 5000},
-  {id: 4, title: 'Gamepad', price: 4500},
-];
+const API = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses';
 
-const renderProduct = (title, price, id, img = 'https://liftshop24.ru/custom/custom31_07_2018_15_15/img/not-image.png') => {
-  return `<div class="product__item">
-            <img class="product__img" src="${img}" alt="" />
-            <h3 class="product__title">${title}</h3>
-            <p class="product__title">${price}</p>
-            <button data-id="${id}" class="btn btn__add">Добавить в корзину</button>
-          </div>`;
-};
+const app = new Vue({
+    el: '#app',
+    data: {
+        catalogUrl: '/catalogData.json',
+        products: [],
+        imgCatalog: 'https://liftshop24.ru/custom/custom31_07_2018_15_15/img/not-image.png',
+        imgCatalogSmall: 'https://liftshop24.ru/custom/custom31_07_2018_15_15/img/not-image.png',
+        filteredGoods: [],
+        searchLine: '',
+        isVisible: false
 
-const renderProducts = list => {
-  const productList = list.map((product) => {
-      return renderProduct(product.title, product.price, product.id);
-  });
-  // console.log(productList);
-  document.querySelector('.products').innerHTML = productList.join('');
-}
+    },
+    methods: {
+        getJson(url){
+            return fetch(url)
+                .then(result => result.json())
+                .catch(error => {
+                    console.log(error);
+                })
+        },
+        addProduct(product){
+            console.log(product.id_product);
+        },
+        FilterGoods() {
+            const regexp = new RegExp(this.searchLine, 'i');
+            this.filteredGoods = this.products.filter(product => {
+                return regexp.test(product.product_name);
+            });
+        }
+    },
+    mounted(){
+       this.getJson(`${API + this.catalogUrl}`)
+           .then(data => {
+               for(let el of data){
+                   this.products.push(el);
+                   this.filteredGoods.push(el)
+               }
+           });
+        this.getJson(`getProducts.json`)
+            .then(data => {
+                for(let el of data){
+                    this.products.push(el);
+                    this.filteredGoods.push(el)
+                }
+            })
 
-renderProducts(products);
+
+    },
+    computed: {
+        divClasses() {
+            return {
+                invisible: !this.isVisible,
+                '': this.isVisible
+            }
+        }
+    }
+})
